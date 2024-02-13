3db08d37535a3c450df118b522f7a1b21a744260
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-14 10:53:32 +1100

    init donera dapp package

---
+++ packages/dapp/src/contracts/fund.test.ts
@@ -0,0 +1,91 @@
+import { describe, it, beforeAll, expect } from "bun:test";
+import { randomContractAddress, testAddress } from "@alephium/web3-test";
+import { expectAssertionError, prepareForTests } from "./utils.test";
+import { Fund } from "./fund";
+import { ALPH_TOKEN_ID, ONE_ALPH } from "@alephium/web3";
+
+describe("Fund", () => {
+  beforeAll(async () => {
+    await prepareForTests();
+  });
+  describe("donate()", () => {
+    it("should fail when called by an address that isn't donera", async () => {
+      const address = randomContractAddress();
+      const call = () =>
+        Fund.tests.donate({
+          address,
+          initialFields: {
+            ...Fund.getInitialFieldsWithDefaultValues(),
+            selfOwner: randomContractAddress(),
+          },
+          testArgs: { donor: testAddress, tokenId: ALPH_TOKEN_ID, amount: ONE_ALPH },
+          inputAssets: [{ address: testAddress, asset: { alphAmount: ONE_ALPH * 200n } }],
+        });
+      await expectAssertionError(call, address, Fund.consts.OwnedError.Forbidden);
+    });
+    it("should transfer the expected tokens to self", async () => {
+      const address = randomContractAddress();
+      const initialAlph = ONE_ALPH;
+      const donatedAlph = ONE_ALPH * 5n;
+      const result = await Fund.tests.donate({
+        address,
+        initialAsset: { alphAmount: initialAlph },
+        initialFields: {
+          ...Fund.getInitialFieldsWithDefaultValues(),
+          selfOwner: testAddress,
+        },
+        testArgs: { donor: testAddress, tokenId: ALPH_TOKEN_ID, amount: donatedAlph },
+        inputAssets: [{ address: testAddress, asset: { alphAmount: ONE_ALPH * 200n } }],
+      });
+      const contract = result.contracts.find((c) => c.address === address);
+      expect(contract!.asset.alphAmount).toBe(initialAlph + donatedAlph);
+    });
+  });
+  describe("finalize()", () => {
+    it("should fail when called by an address that isn't donera", async () => {
+      const address = randomContractAddress();
+      const call = () =>
+        Fund.tests.finalize({
+          address,
+          initialFields: {
+            ...Fund.getInitialFieldsWithDefaultValues(),
+            selfOwner: randomContractAddress(),
+          },
+          inputAssets: [{ address: testAddress, asset: { alphAmount: ONE_ALPH * 200n } }],
+        });
+      await expectAssertionError(call, address, Fund.consts.OwnedError.Forbidden);
+    });
+    it("should fail if fund raising is still in progress", async () => {
+      const address = randomContractAddress();
+      const call = () =>
+        Fund.tests.finalize({
+          address,
+          blockTimeStamp: 5,
+          initialFields: {
+            ...Fund.getInitialFieldsWithDefaultValues(),
+            selfDeadline: 6n,
+            selfOwner: testAddress,
+          },
+          inputAssets: [{ address: testAddress, asset: { alphAmount: ONE_ALPH * 200n } }],
+        });
+      await expectAssertionError(call, address, Fund.consts.FundError.FundInProgress);
+    });
+    // currently throwing an error about a contract not existing
+    it.todo("should transfer assets to recipient", async () => {
+      const address = randomContractAddress();
+      const result = await Fund.tests.finalize({
+        address,
+        blockTimeStamp: 7,
+        initialAsset: { alphAmount: ONE_ALPH },
+        initialFields: {
+          ...Fund.getInitialFieldsWithDefaultValues(),
+          selfRecipient: testAddress,
+          selfDeadline: 6n,
+          selfOwner: testAddress,
+        },
+        inputAssets: [{ address: testAddress, asset: { alphAmount: ONE_ALPH * 200n } }],
+      });
+      console.log(result);
+    });
+  });
+});
---
+++ packages/dapp/test/e2e/donera.test.ts
@@ -0,0 +1,94 @@
+// VERY rough end-to-end test for fund creation -> donation -> fund finalization
+import { describe, it } from "bun:test";
+import { testNodeWallet } from "@alephium/web3-test";
+import { CreateFund, DonateToFund, Donera, DoneraTypes, FinalizeFund } from "../../src";
+import { loadDeployments } from "../../artifacts/ts/deployments";
+import {
+  ONE_ALPH,
+  stringToHex,
+  DUST_AMOUNT,
+  web3,
+  Contract,
+  Project,
+  addressFromContractId,
+  convertAlphAmountWithDecimals,
+  SignerProvider,
+  ALPH_TOKEN_ID,
+} from "@alephium/web3";
+
+describe("end-to-end Donera", () => {
+  it("should create, donte & finalize funds", async () => {
+    web3.setCurrentNodeProvider("http://127.0.0.1:22973");
+    await Project.build();
+    const signer = (await testNodeWallet()) as unknown as SignerProvider;
+    const account = await signer.getSelectedAccount();
+    const deploys = loadDeployments("devnet");
+    const donera = deploys.contracts.Donera.contractInstance.contractId;
+    const listingFeeCall =
+      await deploys.contracts.Donera.contractInstance.methods.getAttoListingFee();
+    console.log(`Donera address: ${deploys.contracts.Donera.contractInstance.address}`);
+    const result = await CreateFund.execute(signer as unknown as SignerProvider, {
+      initialFields: {
+        donera,
+        name: stringToHex("test fund14"),
+        description: stringToHex("testing a fund raiser"),
+        recipient: account.address,
+        goal: 1n,
+        deadlineTimestamp: 5n,
+      },
+      attoAlphAmount: listingFeeCall.returns + ONE_ALPH + DUST_AMOUNT,
+    });
+    console.log(`create fund txid: ${result.txId}`);
+    const node = web3.getCurrentNodeProvider();
+    const events = await node.events.getEventsTxIdTxid(result.txId);
+    const fundCreatedEvent = events.events.find(
+      (e) => e.eventIndex === Donera.eventIndex.FundListed
+    );
+    if (!fundCreatedEvent) {
+      console.error(events);
+      throw new Error("Couldn't find a fund created event");
+    }
+    const parsedEvent = Contract.fromApiEvent(
+      fundCreatedEvent,
+      Donera.contract.codeHash,
+      result.txId
+    ) as DoneraTypes.FundListedEvent;
+    console.log(parsedEvent.fields);
+    console.log(parsedEvent.fields.fundContractId);
+    // DONATE TO FUND
+    console.log(`donating to address: ${addressFromContractId(parsedEvent.fields.fundContractId)}`);
+    const donateAmount = convertAlphAmountWithDecimals(3)!;
+    const donateResult = await DonateToFund.execute(signer, {
+      initialFields: {
+        donera,
+        fundContractId: parsedEvent.fields.fundContractId,
+        tokenId: ALPH_TOKEN_ID,
+        amount: donateAmount,
+      },
+      attoAlphAmount: donateAmount,
+    });
+    console.log(`donate tx id: ${donateResult.txId}`);
+    const donateEvents = await node.events.getEventsTxIdTxid(donateResult.txId);
+    const donationEvent = donateEvents.events.find(
+      (e) => e.eventIndex === Donera.eventIndex.Donation
+    );
+    if (!donationEvent) {
+      console.error(donateEvents);
+      throw new Error("couldnt find donation event");
+    }
+    const donEvent = Contract.fromApiEvent(
+      donationEvent,
+      Donera.contract.codeHash,
+      result.txId
+    ) as DoneraTypes.DonationEvent;
+    console.log(donEvent);
+    // FINALIZE FUND
+    const finalizeResult = await FinalizeFund.execute(signer, {
+      initialFields: {
+        donera,
+        fundContractId: parsedEvent.fields.fundContractId,
+      },
+    });
+    console.log(`finalize fund tx id: ${finalizeResult.txId}`);
+  });
+});
