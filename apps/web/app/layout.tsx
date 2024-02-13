c7306bad65e6918a80dd04a622102e2ef1f91b1a
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-13 13:27:14 +1100

    add alpheium connect, upgrade packages to fix different types

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,12 +1,16 @@
 import "@mantine/core/styles.css";
 import type { Metadata } from "next";
-
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { AlephiumWalletProvider } from "@alephium/web3-react";
+import { NetworkId } from "@alephium/web3";
 
 export const metadata: Metadata = {
   title: "Donera",
   description: "A decentralized crowdfunding platform built on Alephium",
 };
+
+// TODO: should validate this
+const networkId = process.env.NEXT_PUBLIC_DONERA_NETWORK as NetworkId;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <AlephiumWalletProvider network={networkId}>{children}</AlephiumWalletProvider>
+        </MantineProvider>
       </body>
     </html>
   );
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button, Title } from "@mantine/core";
+import { AlephiumConnectButton } from "@alephium/web3-react";
 
 export default function Home() {
   return (
     <div>
       <Title order={1}>Testing mantine</Title>
       <Button>Test button</Button>
+      <AlephiumConnectButton />
     </div>
   );
 }
