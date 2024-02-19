ac249d449738e63a1a1c89407af6fe2b871dd0d1
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-19 21:56:11 +1100

    fetch balances in list page

--- apps/web/app/browse/page.tsx
+++ apps/web/app/browse/page.tsx
@@ -1,13 +1,19 @@
 import { Center, Container, Group, Space, Title } from "@mantine/core";
 import db from "@donera/database";
+import { FundWithBalance, fundsWithBalances } from "@donera/database/funds";
 import classes from "./page.module.css";
 import { FundCard } from "./_components/FundCard";
+import { ALPH_TOKEN_ID } from "@alephium/web3";
 
 // ensure we always get up-to-date funds
 export const dynamic = "force-dynamic";
 
+function alphRaised(fund: FundWithBalance): string {
+  return fund.balances.find((b) => b.tokenId === ALPH_TOKEN_ID)?.balance ?? "0";
+}
+
 export default async function BrowseFundsPage() {
-  const funds = await db.fund.findMany();
+  const funds = await fundsWithBalances(db);
 
   return (
     <Container className={classes.container} size={1000}>
@@ -17,7 +23,7 @@ export default async function BrowseFundsPage() {
       <Space h="xl" />
       <Group justify="space-between">
         {funds.map((f) => (
-          <FundCard key={f.id} fund={f} alphRaised="0" miw={400} />
+          <FundCard key={f.id} fund={f} alphRaised={alphRaised(f)} miw={400} />
         ))}
       </Group>
     </Container>
