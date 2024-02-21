347f249a2a85661a37d1be4145c2a86a41cd8bf8
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-21 23:15:26 +1100

    empty browse page placeholder

--- apps/web/app/browse/page.tsx
+++ apps/web/app/browse/page.tsx
@@ -1,8 +1,9 @@
-import { Center, Container, Group, Space, Title } from "@mantine/core";
+import { Center, Container, Group, Space, Title, Text } from "@mantine/core";
 import db, { Fund } from "@donera/database";
 import classes from "./page.module.css";
 import { FundCard } from "./_components/FundCard";
 import { ALPH_TOKEN_ID } from "@alephium/web3";
+import { IconGift } from "@tabler/icons-react";
 
 // ensure we always get up-to-date funds
 export const dynamic = "force-dynamic";
@@ -11,8 +12,27 @@ function alphRaised(fund: Fund): string {
   return fund.balances?.find((b) => b.id === ALPH_TOKEN_ID)?.amount ?? "0";
 }
 
+function EmptyPlaceholder() {
+  return (
+    <Center
+      bg="var(--mantine-color-gray-light)"
+      mih={400}
+      styles={{ root: { textAlign: "center" } }}
+    >
+      <span>
+        <IconGift size={32} stroke={1} />
+        <Text>No Fundraisers active</Text>
+      </span>
+    </Center>
+  );
+}
+
 export default async function BrowseFundsPage() {
-  const funds = await db.fund.findMany({});
+  const funds = await db.fund.findMany({
+    where: {
+      verified: true,
+    },
+  });
 
   return (
     <Container className={classes.container} size={1000}>
@@ -20,11 +40,15 @@ export default async function BrowseFundsPage() {
         <Title order={1}>Browse fundraisers listed on Donera</Title>
       </Center>
       <Space h="xl" />
-      <Group justify="space-between">
-        {funds.map((f) => (
-          <FundCard key={f.id} fund={f} alphRaised={alphRaised(f)} miw={400} />
-        ))}
-      </Group>
+      {!funds.length ? (
+        <EmptyPlaceholder />
+      ) : (
+        <Group justify="space-between">
+          {funds.map((f) => (
+            <FundCard key={f.id} fund={f} alphRaised={alphRaised(f)} miw={400} />
+          ))}
+        </Group>
+      )}
     </Container>
   );
 }
