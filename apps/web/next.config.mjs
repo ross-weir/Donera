30ee2c695046cc1c47c7824e6420e179d599d104
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-23 07:28:05 +1100

    fix prisma+nextjs+monorepo issue

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,13 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
+    }
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
     }
     return config;
   },
