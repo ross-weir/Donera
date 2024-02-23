a1336b0be247b6cdb64f07783304bc6822be1022
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-23 18:01:57 +1100

    update indexer call

--- packages/indexer/src/simple-event-indexer.ts
+++ packages/indexer/src/simple-event-indexer.ts
@@ -122,10 +122,11 @@ export class SimpleEventIndexer extends BaseIndexer {
   private async processDonation(event: DoneraTypes.DonationEvent): Promise<Tx[]> {
     const { fundContractId, amount, tokenId, ...rest } = event.fields;
     const contractAddress = addressFromContractId(fundContractId);
-    const group = this.deploys.contracts.Donera.contractInstance.groupIndex;
-    const { asset } = await this.getContractState(contractAddress, {
-      group,
-    });
+    // const group = this.deploys.contracts.Donera.contractInstance.groupIndex;
+    const { asset } = await this.node.contracts.getContractsAddressState(contractAddress);
+    // // const { asset } = await this.getContractState(contractAddress, {
+    //   group,
+    // });
     const balances = [{ id: ALPH_TOKEN_ID, amount: asset.attoAlphAmount }, ...(asset.tokens ?? [])];
     return [
       this.db.donation.create({
