2f0b898278a93e1054c210ef636f4137068b7e08
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-02-24 20:17:55 +1100

    also store url

--- apps/web/app/create/_actions/mutations.ts
+++ apps/web/app/create/_actions/mutations.ts
@@ -5,11 +5,14 @@ import { CreateFundResult } from "@donera/dapp";
 import db from "@donera/database";
 import { nanoid } from "nanoid";
 import { blob } from "@/_lib/server";
+import { cidToUrl } from "@/_lib/donera";
 
 export async function saveFund({ fundContractId, ...rest }: CreateFundResult, formData: FormData) {
   const file = formData.get("image") as File;
   const { id } = await blob.put(fundContractId, await file.arrayBuffer());
-  const metadata = { image: { cid: id } };
+  // storing url to ensure there's a way to get the image until the cid is stored onchain
+  // https://github.com/ross-weir/Donera/issues/73
+  const metadata = { image: { cid: id, url: cidToUrl(id) } };
 
   // there's a race condition where the indexer can insert the record before
   // we have a chance to optimistically create it. Use upsert.
